6698acecaf53b134af8fe533ddecd27985dd5ed0
Luiz Fernando <fernando.spyke@gmail.com>
2026-03-19 16:58:40 -0300

    feat: implementar cadastro completo, filtros temporais e exclusão
    - Adição de formulário dinâmico com suporte a novos tipos de dados (Data e Categoria).
    - Unificação das tabelas de transação com lógica de filtragem por tipo.
    - Implementação de sistema de filtros dinâmicos por Ano e Mês.
    - Adição de funcionalidade de exclusão de lançamentos individuais via ID.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const entradaSalvos = localStorage.getItem("listaEntrada");
-const saidaSalvos = localStorage.getItem("listaSaida");
-const totalSalvos = localStorage.getItem("listaTotal");
-const listaEntrada = entradaSalvos ? JSON.parse(entradaSalvos) : [];
-const listaSaida = saidaSalvos ? JSON.parse(saidaSalvos) : [];
-const listaTotal = totalSalvos ? JSON.parse(totalSalvos) : [];
+let transacoesSalvas = [];
+
+const anoFiltro = document.getElementById("btnAno");
+const mesFiltro = document.getElementById("btnMes");
+
+function atualizaLocal(novaTransacao) {
+  transacoesSalvas.push(novaTransacao);
+
+  localStorage.setItem("extrato", JSON.stringify(transacoesSalvas));
+}
 
 const formatarMoeda = (valor) => {
   return valor.toLocaleString("pt-BR", {
@@ -16,91 +20,221 @@ function reset() {
   localStorage.clear();
 }
 
-function addEntrada() {
-  const input = document.getElementById("entrada");
-  const valor = Number(input.value);
+const form = document.getElementById("transacaoForm");
 
-  if (valor > 0) {
-    listaEntrada.push(valor);
-    listaTotal.push(valor);
-    atualizarTela();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const valorBruto = document.getElementById("valor").value;
+
+  const valorNumerico = parseFloat(
+    valorBruto.replace(/[^\d,]/g, "").replace(",", "."),
+  );
+
+  let dataPreenchida = document.getElementById("data").value;
+
+  if (!dataPreenchida) {
+    const hoje = new Date();
+    dataPreenchida = hoje.toISOString().split("T")[0];
   }
-}
 
-function addSaida() {
-  const input = document.getElementById("saida");
-  const valor = Number(input.value);
+  const novaTransacao = {
+    id: Date.now(),
+    descricao: document.getElementById("descricao").value,
+    valor: valorNumerico,
+    data: dataPreenchida,
+    tipo: document.getElementById("tipo").value,
+    categoria: document.getElementById("categoria").value,
+  };
+  atualizaLocal(novaTransacao);
 
-  if (valor > 0) {
-    listaSaida.push(valor);
-    listaTotal.push(-valor);
-    atualizarTela();
-  }
-}
+  form.reset();
+
+  atualizarTela();
+  alert("Transação salva com sucesso!");
+});
 
 function atualizarTela() {
-  localStorage.setItem("listaEntrada", JSON.stringify(listaEntrada));
-  localStorage.setItem("listaSaida", JSON.stringify(listaSaida));
-  localStorage.setItem("listaTotal", JSON.stringify(listaTotal));
-  atualizarEntradas();
-  atualizarSaidas();
-  atualizarTotal();
-}
-
-function atualizarEntradas() {
   const tabelaEntrada = document.getElementById("tEntrada");
   const totalEntrada = document.getElementById("totalEntrada");
-
-  tabelaEntrada.innerHTML = listaEntrada
-    .map((item) => {
-      const valorFormatado = formatarMoeda(item);
-      return `<tr><td>${valorFormatado}</td></tr>`;
-    })
-    .join("");
-
-  const somaTotal = listaEntrada.reduce((acumulador, numeroTotal) => {
-    return acumulador + Number(numeroTotal);
-  }, 0);
-
-  totalEntrada.innerText = formatarMoeda(somaTotal);
-}
-
-function atualizarSaidas() {
   const tabelaSaida = document.getElementById("tSaida");
   const totalSaida = document.getElementById("totalSaida");
-
-  tabelaSaida.innerHTML = listaSaida
-    .map((item) => {
-      const valorFormatado = formatarMoeda(item);
-      return `<tr><td>${valorFormatado}</td></tr>`;
-    })
-    .join("");
-
-  const somaTotal = listaSaida.reduce((acumulador, numeroTotal) => {
-    return acumulador + Number(numeroTotal);
-  }, 0);
-
-  totalSaida.innerText = formatarMoeda(somaTotal);
-}
-
-function atualizarTotal() {
-  const tabelaTotal = document.getElementById("tLancamentos");
+  const tabelaLancamento = document.getElementById("tLancamento");
   const totalLancamento = document.getElementById("totalLancamento");
+  const listaAtual = transacoesSalvas.filter(filtrarAno).filter(filtrarMes);
 
-  tabelaTotal.innerHTML = listaTotal
-    .map((item) => {
-      const classCor = item > 0 ? "positivo" : item < 0 ? "negativo" : "";
+  const linhasTabelaEntrada = gerarLinhasTabela(
+    listaAtual.filter((item) => item.tipo === "entrada"),
+  );
+  const resultadoEntrada = somaTotal(
+    listaAtual.filter((item) => item.tipo === "entrada"),
+  );
 
-      const valorFormatado = formatarMoeda(item);
-      return `<tr><td class="${classCor}" >${valorFormatado}</td></tr>`;
-    })
-    .join("");
+  const linhasTabelaSaida = gerarLinhasTabela(
+    listaAtual.filter((item) => item.tipo === "saida"),
+  );
+  const resultadoSaida = somaTotal(
+    listaAtual.filter((item) => item.tipo === "saida"),
+  );
 
-  const somaTotal = listaTotal.reduce((acumulador, numeroTotal) => {
-    return acumulador + Number(numeroTotal);
-  }, 0);
+  const linhasTabelaTotal = gerarLinhasTabela(listaAtual);
+  const resultadoTotal = somaTotal(listaAtual);
 
-  totalLancamento.innerText = formatarMoeda(somaTotal);
+  tabelaEntrada.innerHTML = linhasTabelaEntrada;
+  totalEntrada.innerText = formatarMoeda(resultadoEntrada);
+  tabelaSaida.innerHTML = linhasTabelaSaida;
+  totalSaida.innerText = formatarMoeda(resultadoSaida);
+  tabelaLancamento.innerHTML = linhasTabelaTotal;
+  totalLancamento.innerText = formatarMoeda(resultadoTotal);
 }
 
-atualizarTela();
+function somaTotal(lista) {
+  return lista.reduce((acumulador, objetoAtual) => {
+    return acumulador + Number(objetoAtual.valor);
+  }, 0);
+}
+
+function gerarLinhasTabela(lista) {
+  return lista
+    .map((item) => {
+      const valorFormatado = formatarMoeda(item.valor);
+      const classCor = item.tipo;
+
+      return `
+    <tr class="${classCor}">
+      <td>${item.descricao}</td>
+      <td>${valorFormatado}</td>
+      <td>${item.data}</td>
+      <td>${item.tipo}</td>
+      <td>${item.categoria}</td>
+      <td><button onclick="removerItem(${item.id})">X</button></td>
+      </tr>
+  `;
+    })
+    .join("");
+}
+
+function removerItem(idParaRemover) {
+  transacoesSalvas = transacoesSalvas.filter(
+    (item) => item.id !== idParaRemover,
+  );
+  localStorage.setItem("extrato", JSON.stringify(transacoesSalvas));
+  atualizarTela();
+}
+
+function mascaraMoeda(input) {
+  let valor = input.value;
+
+  valor = valor.replace(/\D/g, "");
+
+  valor = formatarMoeda(valor / 100);
+
+  input.value = valor;
+}
+
+function filtrarMes(item) {
+  const dataObj = new Date(item.data);
+  return dataObj.getMonth() === Number(mesCentro);
+}
+
+function filtrarAno(item) {
+  const dataObj = new Date(item.data);
+  return dataObj.getFullYear() === Number(anoCentro);
+}
+
+function atualizarFiltro() {
+  const hoje = new Date(Date.now());
+  const mesHoje = `${hoje.getMonth()}`;
+  const anoHoje = `${hoje.getFullYear()}`;
+  mesFiltro.dataset.mes = mesHoje;
+  mesFiltro.innerText = convertMes(mesHoje);
+  anoFiltro.innerText = anoHoje;
+}
+
+function convertMes(valor) {
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outrubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  return meses[valor];
+}
+
+atualizarFiltro();
+const menuFiltroAno = document.querySelector("#cabecalhoAno");
+const menuFiltroMes = document.querySelector("#cabecalhoMes");
+let anoCentro = anoFiltro.innerText;
+let mesCentro = mesFiltro.dataset.mes;
+
+function renderizarBotoesAno(anoFoco) {
+  menuFiltroAno.innerHTML = "";
+
+  const offsets = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6];
+
+  offsets.forEach((offset) => {
+    const ano = parseInt(anoFoco) + offset;
+    const btn = document.createElement("button");
+    btn.innerText = ano;
+    btn.dataset.ano = ano;
+
+    if (offset == 0) btn.style.fontWeight = "bold";
+
+    menuFiltroAno.appendChild(btn);
+  });
+}
+
+function renderizarBotoesMes(mesFoco) {
+  menuFiltroMes.innerHTML = "";
+
+  const offsets = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+
+  offsets.forEach((offset) => {
+    const mes = (parseInt(mesFoco) + offset + 12) % 12;
+    const btn = document.createElement("button");
+    btn.innerText = convertMes(mes);
+    btn.dataset.mes = mes;
+
+    if (offset == 0) btn.style.fontWeight = "bold";
+
+    menuFiltroMes.appendChild(btn);
+  });
+}
+
+menuFiltroAno.addEventListener("click", (event) => {
+  const elemento = event.target;
+  if (elemento.tagName === "BUTTON") {
+    const anoSelecionado = parseInt(elemento.dataset.ano);
+    console.log(anoSelecionado);
+    anoCentro = anoSelecionado;
+    renderizarBotoesAno(anoSelecionado);
+    atualizarTela();
+  }
+});
+
+menuFiltroMes.addEventListener("click", (event) => {
+  const elemento = event.target;
+  if (elemento.tagName === "BUTTON") {
+    const mesSelecionado = parseInt(elemento.dataset.mes);
+    mesCentro = mesSelecionado;
+    renderizarBotoesMes(mesSelecionado);
+    atualizarTela();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  transacoesSalvas = JSON.parse(localStorage.getItem("extrato")) || [];
+  atualizarFiltro();
+  atualizarTela();
+  renderizarBotoesAno(anoCentro);
+  renderizarBotoesMes(mesCentro);
+});
